01a6460e36a0dc06466e15dfe9cb105a9c9a111b
Sergei Krainyukov <sk-new@mail.ru>
2024-06-24 21:20:25 +0300

    task4

--- src/task4/Main.java
+++ src/task4/Main.java
@@ -1,10 +1,32 @@
 package task4;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static boolean is(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s.length() % 2 != 0) return false;
+        for (int i = 0; i < s.length(); i++) {
+            stack.push(s.charAt(i));
+        }
+        int stackLength = stack.size();
+        int count = 0;
+        for (int i = 0; i < stackLength; i++) {
+            Character character = stack.pop();
+            if (count < 0) return false;
+            if (i <= 1 && character != ')') return false;
+            if (i == 0) continue;
+            if (i == 1) {
+                count++;
+                continue;
+            }
+            if (i >= stackLength - 2 && character != '(') return false;
+            if (i == stackLength - 2) {
+                count--;
+                continue;
+            }
+            if (i == stackLength - 1) continue;
+            count = character != ')' ? count - 1 : count + 1;
+        }
+        return count == 0;
     }
-//    public boolean is(String s){
-//
-//    }
 }
+
--- src/task4/Stack.java
+++ src/task4/Stack.java
@@ -1,3 +1,5 @@
+package task4;
+
 import java.util.*;
 
 public class Stack<T> {
