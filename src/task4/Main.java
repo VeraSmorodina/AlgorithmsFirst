440f7cfdd328b8789911f4aa5268737ed1369256
Sergei Krainyukov <sk-new@mail.ru>
2024-06-25 15:38:13 +0300

    task4

--- src/task4/Main.java
+++ src/task4/Main.java
@@ -1,32 +1,22 @@
 package task4;
 
 public class Main {
-    public static boolean is(String s) {
+    public static boolean isBalanced(String s) {
         Stack<Character> stack = new Stack<>();
-        if (s.length() % 2 != 0) return false;
         for (int i = 0; i < s.length(); i++) {
-            stack.push(s.charAt(i));
-        }
-        int stackLength = stack.size();
-        int count = 0;
-        for (int i = 0; i < stackLength; i++) {
-            Character character = stack.pop();
-            if (count < 0) return false;
-            if (i <= 1 && character != ')') return false;
-            if (i == 0) continue;
-            if (i == 1) {
-                count++;
+            char elem = s.charAt(i);
+            if (elem == '(') {
+                stack.push(elem);
                 continue;
             }
-            if (i >= stackLength - 2 && character != '(') return false;
-            if (i == stackLength - 2) {
-                count--;
-                continue;
+            if (elem == ')' && stack.size() == 0) {
+                return false;
             }
-            if (i == stackLength - 1) continue;
-            count = character != ')' ? count - 1 : count + 1;
+            if (elem == ')') {
+                stack.pop();
+            }
         }
-        return count == 0;
+        return stack.size() == 0;
     }
 }
 
